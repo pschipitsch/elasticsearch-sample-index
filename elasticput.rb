77d3a578fe4231f198436c12942f598410fec13a
Paul Schipitsch <pschipitsch@truecar.com>
2016-04-12 22:25:55 -0700

    Use net/http

--- elasticput.rb
+++ elasticput.rb
@@ -1,6 +1,22 @@
 require 'csv'
+require 'json'
+require 'net/http'
+require 'uri'
+
+
+uri     = URI.parse("http://#{ARGV[0]}/comicbook/superhero")
+headers = {'Content-Type' => "application/json",
+           'Accept'       => "application/json"}
+http    = Net::HTTP.new(uri.host, uri.port)
+request = Net::HTTP::Post.new(uri.request_uri, headers)
 
 arr_of_arrs = CSV.read("indexme.csv")
 arr_of_arrs.each do |row|
-  `curl -XPOST http://#{ARGV[0]}/comicbook/superhero/ -d '{"name" : "#{row.first}", "summary" : "#{row.last}" }'`
+  data = {
+    name:    row.first,
+    summary: row.last
+  }
+  request.body = data.to_json
+  response     = http.request(request)
+  puts response.body
 end
